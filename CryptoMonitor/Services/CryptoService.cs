0d0a7b3c8f81ab6acbf598decefe372230b4d174
Dekkert Bence <231654321+Dekii24@users.noreply.github.com>
2026-05-17 00:43:39 +0200

    add CryptoService for external API communication

---
+++ CryptoMonitor/Services/CryptoService.cs
@@ -0,0 +1,35 @@
+﻿using CryptoMonitor.Models;
+using System.Text.Json;
+
+namespace CryptoMonitor.Services
+{
+    public class CryptoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CryptoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<CryptoResponse?> GetPricesAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd");
+                response.EnsureSuccessStatusCode();
+
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                var data = JsonSerializer.Deserialize<CryptoResponse>(jsonString);
+
+                return data;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred while retrieving data: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
